90001100caac0d68d3efafc6ae3768f4e47cc005
Ben Krikler <bek07@ic.ac.uk>
2019-03-01 10:02:21 +0100

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.cern.ch/fast-hep/public/fast_plotter',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
