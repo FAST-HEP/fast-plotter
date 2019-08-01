b46fdb56d7fa5b828d43c17f8fd7e8193a0d090b
Ben Krikler <bek07@ic.ac.uk>
2019-08-01 21:12:37 +0200

    Add scipy as a requirement

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 #with open('HISTORY.rst') as history_file:
 #    history = history_file.read()
 
-requirements = ['matplotlib<3', 'pandas', 'numpy']
+requirements = ['matplotlib<3', 'pandas', 'numpy', 'scipy']
 
 setup_requirements = ['pytest-runner', ]
 
