e8b2effdf5e05c7d5a85e9802cba824de6d40e34
Jordan Palmer <quarkyjordan@gmail.com>
2019-10-07 06:59:46 -0700

    added log z boolean as an argument

--- fast_plotter/plotting_2d.py
+++ fast_plotter/plotting_2d.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 #might want to eventually add a z scale option
 #
 def plot_all_2d(df, project_1d=True, project_2d=True, data="data", signal=None, dataset_col="dataset",
-             yscale="log", lumi=None, annotations=[], dataset_order="sum-ascending",
+                yscale="log", zscale=False, lumi=None, annotations=[], dataset_order="sum-ascending",
              continue_errors=True, bin_variable_replacements={}, **kwargs):
 
     figures = {}
@@ -17,7 +17,7 @@ def plot_all_2d(df, project_1d=True, project_2d=True, data="data", signal=None, 
     
     if len(dimensions) == 2: #if there is no dataset specified in df
         df = utils.rename_index(df, bin_variable_replacements)
-        figures[(("yscale", yscale),)] = plot_2d(df)
+        figures[(("yscale", yscale),)] = plot_2d(df,zscale)
 
     if dataset_col in dimensions: # check how many dimensions there are excluding dataset 
         dimensions = tuple(dim for dim in dimensions if dim != dataset_col)
@@ -25,7 +25,7 @@ def plot_all_2d(df, project_1d=True, project_2d=True, data="data", signal=None, 
     if project_2d and len(dimensions) > 2: #if we want a 2d hist and the dimensions are >2
         # this will be more complicated once we add the ability to include multiple datasets 
         df = utils.rename_index(df, bin_variable_replacements)
-        figures[(("yscale", yscale),)] = plot_2d(df)
+        figures[(("yscale", yscale),)] = plot_2d(df,zscale)
 
     return figures, ran_ok
         
@@ -33,11 +33,13 @@ def plot_2d_many():
     print("many")
 
 
-def plot_2d(df):
+def plot_2d(df, log=False):
     import seaborn as sns
+    from matplotlib.colors import LogNorm
     dimensions = utils.binning_vars(df) # tuple of column names 
     df.reset_index(inplace=True) # comvert from multiindex to columns
     reshaped = df.pivot(dimensions[2],dimensions[1],dimensions[3]) # reshape columns and rows [y,x,n]
     f,ax = plt.subplots() # can be generalised for multiple datasets
-    sns.heatmap(reshaped,annot=True,ax=ax) # annot could take annotations array? 
+    sns.heatmap(reshaped,annot=True,ax=ax) # annot could take annotations array?
     return f
+ # norm=LogNorm()
