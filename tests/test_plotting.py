dd5a0d504e92e5ef6744ff7a8fa8a2fe7ae366d9
Ben Krikler <bek07@ic.ac.uk>
2020-05-22 00:35:52 +0200

    Fix unit tests

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -22,19 +22,20 @@ def test_replace_inf():
 
 def test_pad_zero_noYs():
     x = np.arange(5)
-    padded, = plotting.standardize_values(x)
+    padded, ticks = plotting.standardize_values(x)
     assert np.array_equal(padded, np.arange(-1, 6))
+    assert ticks is None
 
     x = np.concatenate(([-np.inf], x, [np.inf]), axis=0)
-    padded, = plotting.standardize_values(x)
+    padded, _ = plotting.standardize_values(x)
     assert np.array_equal(padded, np.arange(-2, 7))
 
     x = np.arange(2, 4)
-    padded, = plotting.standardize_values(x)
+    padded, _ = plotting.standardize_values(x)
     assert np.array_equal(padded, np.arange(1, 5))
 
     x = np.concatenate(([-np.inf], x), axis=0)
-    padded, = plotting.standardize_values(x)
+    padded, _ = plotting.standardize_values(x)
     assert np.array_equal(padded, np.arange(0, 5, dtype=float))
 
 
@@ -42,28 +43,29 @@ def test_pad_zero_oneY():
     x = np.arange(5)
     y = np.arange(5, 0, -1)
     expected_y = np.concatenate(([0], y, [0]), axis=0)
-    pad_x, pad_y = plotting.standardize_values(x, [y])
+    pad_x, ticks, pad_y = plotting.standardize_values(x, [y])
     assert np.array_equal(pad_x, np.arange(-1, 6))
     assert np.array_equal(pad_y, expected_y)
+    assert ticks is None
 
     x = np.concatenate(([-np.inf], x, [np.inf]), axis=0)
     y = np.arange(len(x), 0, -1)
     expected_y = np.concatenate(([0], y, [0]), axis=0)
-    pad_x, pad_y = plotting.standardize_values(x, [y])
+    pad_x, _, pad_y = plotting.standardize_values(x, [y])
     assert np.array_equal(pad_x, np.arange(-2, 7))
     assert np.array_equal(pad_y, expected_y)
 
     x = np.arange(2, 4)
     y = np.arange(len(x), 0, -1)
     expected_y = np.concatenate(([0], y, [0]), axis=0)
-    pad_x, pad_y = plotting.standardize_values(x, [y])
+    pad_x, _, pad_y = plotting.standardize_values(x, [y])
     assert np.array_equal(pad_x, np.arange(1, 5))
     assert np.array_equal(pad_y, expected_y)
 
     x = np.concatenate(([-np.inf], x), axis=0)
     y = np.arange(len(x), 0, -1)
     expected_y = np.concatenate(([0], y, [0]), axis=0)
-    pad_x, pad_y = plotting.standardize_values(x, [y])
+    pad_x, _, pad_y = plotting.standardize_values(x, [y])
     assert np.array_equal(pad_x, np.arange(0, 5))
     assert np.array_equal(pad_y, expected_y)
 
