25d87bb481aff6400b78845d0ac81f0c04b1f04a
David Anthony <danthony@cern.ch>
2021-07-09 12:51:10 +0100

    pep8

--- fast_plotter/utils.py
+++ fast_plotter/utils.py
@@ -102,10 +102,12 @@ def calculate_error(df, sumw2_label="sumw2", err_label="err", inplace=True, do_r
             errs = np.true_divide(df[column], root_n)
             errs.loc[~np.isfinite(errs)] = np.nan
             df[err_name] = errs
-        else:
-        #elif not do_rel_err and sumw2_label in column:
+        elif not do_rel_err and sumw2_label in column:
             err_name = column.replace(sumw2_label, err_label)
             df[err_name] = np.sqrt(df[column])
+        else:
+            err_name = ""
+            continue
         if is_null_poissonian:
             df[err_name] = df[err_name].apply(lambda x: x if x > 1.15 else np.sqrt(1.15**2+x**2))
     if not inplace:
